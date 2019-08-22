0a3bab811d292c0adb95866fc549f6d4255e9639
Keegan Kelly <keegankelly1@gmail.com>
2019-08-22 12:37:00 -0500

    Added hunger, weight and happiness to Pet state. Added eatFood function to Pet.

--- pet/src/Components/Pet.js
+++ pet/src/Components/Pet.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 class Pet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hunger: 0,
+            weight: 0,
+            happiness: 0
+        }
+    }
+
+    eatFood(food) {
+        const newHunger = this.state.hunger - food;
+        const newWeight = this.state.weight + food;
+        this.setState({
+            hunger: newHunger,
+            weight: newWeight
+        })  
+    }
+
     render() {
         return (
             <h1>Hello, I am a Pet</h1>
